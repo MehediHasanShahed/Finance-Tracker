190bf3faa8c86304e231016803c935d50f369dec
Mehedi Hasan Shahed <mehedihasanshahed3@gmail.com>
2026-01-05 18:07:59 +0600

    debugging

--- actions/send-email.js
+++ actions/send-email.js
@@ -1,4 +1,5 @@
 import { Resend } from "resend";
+import { render } from "@react-email/render";
 
 export async function sendEmail({ to, subject, react }) {
     console.log("sendEmail called:", { to, subject, hasReact: !!react });
@@ -7,11 +8,14 @@ export async function sendEmail({ to, subject, react }) {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     try {
+        // Pre-render the React component to HTML for better production compatibility
+        const html = await render(react);
+
         const data = await resend.emails.send({
             from: 'Finance Tracker <onboarding@resend.dev>',
             to,
             subject,
-            react,
+            html,  // Use pre-rendered HTML instead of react prop
         });
 
         console.log("Email sent successfully:", data);
